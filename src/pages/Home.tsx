609b4d0aa65a07fff0bc4649475ee8ec110d7dd6
Cristian Guerrero <cristian.guerrero@uptc.edu.co>
2025-06-04 11:45:56 -0500

    Se maqueta correctamente la homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,12 @@
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div className="flex flex-col gap-4 items-center justify-center h-screen">
+      <h1 className="text-2xl font-semibold text-primary-600">¿Que peronaje quieres ver?</h1>
+      <p className="text-sm text-gray-500">
+        Puedes buscar por nombre, especie o filtrar por personajes favoritos.
+      </p>
+      
+    </div>
   )
 }
