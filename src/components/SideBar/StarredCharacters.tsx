52c85e3a4c665d15bec275222e67a94a8f4e3f1d
Cristian Guerrero <cristian.guerrero@uptc.edu.co>
2025-06-04 11:51:18 -0500

    Se corrige scroll de destacados

--- src/components/SideBar/StarredCharacters.tsx
+++ src/components/SideBar/StarredCharacters.tsx
@@ -19,7 +19,7 @@ export const StarredCharacters = () => {
       <div>
         <div
           className={`flex flex-col ${
-            characters.length > 3 ? "overflow-y-scroll h-[264px] scroll-visible" : ""
+            characters.length > 3 ? "overflow-y-scroll h-[260px] scroll-visible" : ""
           }`}
         >
           {characters.map((character) => (
