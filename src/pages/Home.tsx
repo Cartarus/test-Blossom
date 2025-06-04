52b1dbcbb980217f00421190d0a49633d933dadd
Cristian Guerrero <cristian.guerrero@uptc.edu.co>
2025-06-04 17:36:20 -0500

    Se cambia texto de home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ export const Home = () => {
     <div className="flex flex-col gap-4 items-center justify-center h-full">
       <h1 className="text-2xl font-semibold text-primary-600">¿Qué personaje quieres ver?</h1>
       <p className="text-sm text-gray-500">
-        Puedes buscar por nombre, especie o filtrar por personajes favoritos.
+        Puedes buscar por nombre o filtrar por personajes favoritos ,especie o ordenar por nombre.
       </p>
       
     </div>
